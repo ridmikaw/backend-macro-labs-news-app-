8c2a12e3a9ffd35a73b1b7088c66485bdc90ed78
Ridmi Ranasinghe <it22088246@my.sliit.lk>
2025-05-24 23:55:41 +0530

    Enable the CORS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app.module"
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3001
-  await app.listen(port)
-  console.log(`Backend server running on http://localhost:${port}`)
+  // Enable CORS
+  app.enableCors({
+    origin: "http://localhost:3000", // Frontend URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true, // Allow cookies and credentials
+  });
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Backend server running on http://localhost:${port}`);
 }
 
-bootstrap()
+bootstrap();
